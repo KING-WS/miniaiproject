26aa03fc20580f2fad15970a73da797985fd7a33
DESKTOP-T5O03VS\rladn <rladntjd1193@gmail.com>
2025-10-24 09:50:00 +0900

    1024

---
+++ miniai/src/main/java/edu/sm/MiniaiApplication.java
@@ -0,0 +1,13 @@
+package edu.sm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniaiApplication.class, args);
+    }
+
+}
